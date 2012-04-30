2f4e48d1539bde02d4342344d31331598d71658d
Steve Roettger <steve@steveroettger.com>
2012-04-30 09:24:20 -0400

    Added relationships to patient model

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,28 @@
 class Patient < ActiveRecord::Base
+	has_many				:notifications
+	has_many				:messages, :through => :notifications
+	has_many				:actionconfirmations
+	has_many				:actions, :through => :actionconfirmations
+	has_and_belongs_to_many 	      :addresses
+	
+	has_and_belongs_to_many	:patient groups
+	
+	has_many				:daily_records
+	has_many 				:daily_record_details, :through => :daily_records
+	has_many				:symptoms, :through => :daily_records
+	has_many				:days, :through => :daily_records
+	
+	has_many				:searches
+	has_many				:search phrases, :through => :searches
+	
+	has_many				:notes, :through => :daily_records:
+	
+	has_many				:surveys
+	has_many				:symptoms, :through => :surveys
 
-	attr_accessible :first_name, :last_name, :email, :birthday, :location, :race, :sex, :first_occurrence, :year_diagnosed, 
-	                :condition, :primary_physician, :medications, :other_medications, :photo, :password, :password_confirmation 
+	attr_accessible :first_name, :last_name, :email, :birthday, :location, :race, :sex, 
+					:first_occurrence, :year_diagnosed, :condition, :primary_physician, :medications, 
+					:other_medications, :photo, :password, :password_confirmation 
 		
 	validates :first_name, :presence => true, :length => { :maximum => 50 }
 	validates :last_name,  :presence => true, :length => { :maximum => 50 }
