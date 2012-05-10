b39658ae8a00a5979d2666f151bd5753a37f01bb
Steve Roettger <steve@steveroettger.com>
2012-05-10 10:48:16 -0400

    Completed symptom scaffold

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,2 +1,9 @@
 class Symptom < ActiveRecord::Base
+	belongs_to :symptom_category
+	
+	attr_accessible :name, :description, :symptom_category_id
+	
+	def to_param
+		"#{id} #{name}".parameterize
+	end
 end
