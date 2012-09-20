906f8dc7134adca42102ed460e4f4ede0789d5fb
Steve Roettger <steve@steveroettger.com>
2012-09-20 12:31:41 -0300

    Update config/initializers/setup_mail.rb

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,2 @@
-ActionMailer::Base.smtp_settings = {
-	:address              => "smtp.gmail.com",
-	:port                 => 587,
-	:domain               => "steve.roettger@gmail.com",
-	:user_name            => "steve.roettger",
-	:password             => "sRoettger12:)",
-	:authentication		  => "plain",
-	:enable_starttls_auto => true
-}
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 
